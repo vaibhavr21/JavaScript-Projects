cb6bf0ae057f41f60286e89ee71bfab4c2b0515b
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-05-15 06:20:15 +0530

    scripts File added

---
+++ Project 5/script.js
@@ -0,0 +1,26 @@
+function isPlaindrome(str){
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g,'');
+    console.log(cleanStr);
+
+    const reversedStr = cleanStr.split('').reverse().join('');
+    console.log(reversedStr);
+
+    return cleanStr === reversedStr;
+}
+
+function plaindromeChecker(){
+
+    const inputText = document.getElementById("inputText");
+    const result = document.getElementById("result");
+
+    if(isPlaindrome(inputText.value)){
+        result.textContent = `"${inputText.value}" is a Palindrome`;
+    }else{
+        result.textContent = `"${inputText.value}" is Not a Palindrome`;
+    }
+
+    result.classList.add('fadeIn');
+    inputText.value = "";
+}
+
+document.getElementById('checkButton').addEventListener("click",plaindromeChecker);
