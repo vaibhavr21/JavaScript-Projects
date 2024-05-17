003ffb2c9ee4ecf9e6dd32881ff55c984591bc42
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-05-18 05:25:02 +0530

    script.js file added

---
+++ Project 7/script.js
@@ -0,0 +1,63 @@
+function calculateTip(){
+
+   const billAmountInput = document.getElementById("billAmount");
+   const serviceRatingSelect = document.getElementById("serviceRating");
+   const splitCountInput = document.getElementById("splitCount");
+   const mealTypeSelect = document.getElementById|("mealType");
+
+   const tipAmountOutput = document.getElementById("tipAmount");
+   const totalAmountOutput = document.getElementById("totalAmount");
+   const amountPerPersonOutput = document.getElementById("amountPerPerson");
+
+   const billAmount = parseFloat(billAmountInput.value);
+   const serviceRating = parseFloat(serviceRatingSelect.value);
+   const splitCount = parseFloat(splitCountInput.value);
+   const mealType = mealTypeSelect.value;
+
+   if(isNaN(billAmount) || isNaN(serviceRating) || isNaN(splitCount)){
+    tipAmountOutput.textContent = "Please Enter Valid Number";
+    totalAmountOutput.textContent = "";
+    amountPerPersonOutput.textContent = "";
+    return;
+
+   }
+
+   let tip;
+       switch(serviceRating){
+        case 1:
+            tip = billAmount * 0.05;
+           break;
+        case 2:
+            tip = billAmount * 0.10;
+           break;
+        case 3:
+            tip = billAmount * 0.15;
+           break;
+        case 4:
+            tip = billAmount * 0.20;
+           break;            
+
+       }
+
+       let totalAmount = billAmount + tip;
+       let amountPerPerson = totalAmount / splitCount;
+
+       if(mealType === "dinner"){
+        tip += 5;
+        totalAmount += 5;
+        amountPerPerson += 5;
+       }
+
+       tipAmountOutput.textContent = `Tip: $${tip.toFixed(2)}`;
+       totalAmountOutput.textContent = `Total Amount: $${totalAmount.toFixed(2)}`;
+       amountPerPersonOutput.textContent = `Amount Per Person: $${amountPerPerson.toFixed(2)}`;
+
+
+
+
+
+
+
+}
+
+document.getElementById("calculateBtn").addEventListener("click",calculateTip);
