74e50e861ede55e38d89436a22fd7b34a7829fb9
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-05-19 07:43:12 +0530

    Update script.js

--- Project 7/script.js
+++ Project 7/script.js
@@ -48,9 +48,9 @@ function calculateTip(){
         amountPerPerson += 5;
        }
 
-       tipAmountOutput.textContent = `Tip: $Rs{tip.toFixed(2)}`;
-       totalAmountOutput.textContent = `Total Amount: $Rs{totalAmount.toFixed(2)}`;
-       amountPerPersonOutput.textContent = `Amount Per Person: $Rs{amountPerPerson.toFixed(2)}`;
+       tipAmountOutput.textContent = `Tip: $₹{tip.toFixed(2)}`;
+       totalAmountOutput.textContent = `Total Amount: $₹{totalAmount.toFixed(2)}`;
+       amountPerPersonOutput.textContent = `Amount Per Person: $₹{amountPerPerson.toFixed(2)}`;
 
 
 
